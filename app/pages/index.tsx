3db68ca9c37aace932a87118608d358ae69f8ecc
yukihirop <te108186@gmail.com>
2022-12-04 20:48:34 +0900

    feat: 🎸 impl banner

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -12,10 +12,31 @@ const Container = styled.main`
   /* background: ${(props) => props.theme.colors.neutral.veryPaleRed}; */
 `;
 
-const BigBannerContainer = styled.section`
-  width: 100%;
-  height: 100px;
-  background-color: ${(props) => props.theme.colors.neutral.veryPaleRed};
+const BannerContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100vw;
+  height: 120px;
+  margin: 0 calc(50% - 50vw);
+  margin-top: 6rem;
+  background-color: ${(props) => props.theme.colors.primary.brightRed};
+`;
+
+const BannerTitle = styled.h1`
+  margin: auto;
+  color: white;
+  width: 300px;
+`;
+const BannerButton = styled.button`
+  width: 120px;
+  height: 40px;
+  padding: 0.1rem;
+  color: ${(props) => props.theme.colors.primary.brightRed};
+  background-color: white;
+  border: none;
+  border-radius: 20px;
+  margin: auto;
 `;
 
 export default function Home() {
@@ -25,9 +46,10 @@ export default function Home() {
       <Hero />
       <Article />
       <Recommend />
-      <BigBannerContainer>
-
-      </BigBannerContainer>
+      <BannerContainer>
+        <BannerTitle>Simplify how your team works today.</BannerTitle>
+        <BannerButton>Get Started</BannerButton>
+      </BannerContainer>
     </Container>
   );
 }
