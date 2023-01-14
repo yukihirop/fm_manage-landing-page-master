d874cc03bebd0a3c0c1b52be2c5bba8627131254
yukihirop <te108186@gmail.com>
2023-01-14 14:28:25 +0900

    fix: 🐛 banner

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const BannerContainer = styled.section`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  width: 100vw;
   height: 120px;
-  margin: 0 calc(50% - 50vw);
   margin-top: 6rem;
   background-color: ${(props) => props.theme.colors.primary.brightRed};
 `;
 
+const BannerCenter = styled.div`
+  height: 100%;
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 const BannerTitle = styled.h1`
-  margin: auto;
   color: white;
   width: 300px;
+  margin: auto 0;
 `;
 const BannerButton = styled.button`
   width: 120px;
@@ -25,14 +30,20 @@ const BannerButton = styled.button`
   background-color: white;
   border: none;
   border-radius: 20px;
-  margin: auto;
+  margin: auto 0;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Banner = () => {
   return (
     <BannerContainer>
-      <BannerTitle>Simplify how your team works today.</BannerTitle>
-      <BannerButton>Get Started</BannerButton>
+      <BannerCenter>
+        <BannerTitle>Simplify how your team works today.</BannerTitle>
+        <BannerButton>Get Started</BannerButton>
+      </BannerCenter>
     </BannerContainer>
   );
 };
